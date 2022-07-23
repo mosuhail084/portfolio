9d705f33d84444d77ead81d7ddcaf70d79d78b9e
Mohd Suhail <mosuhail084@gmail.com>
2022-07-23 09:14:36 +0530

    Add files via upload

---
+++ src/data/WorkData.js
@@ -0,0 +1,50 @@
+export const Work = [
+    {
+        id:1,
+        name:"Broadway RC",
+        description:"It is a Visa Provider and consultancy build on top of the Laravel.",
+        tags:["Laravel","Backend"],
+        
+        demo:"https://broadwayrc.com/",
+        github:""
+    },
+    {
+        id:2,
+        name:"Amayra's Store",
+        description:"A E-commerce plateform build in ReactJS. With searching,filtering and many ecom functionalities.",
+        tags:["react","mockbee"],
+        
+        demo:"https://amayrastore.netlify.app/",
+        github:""
+        
+    },
+    {
+        id:3,
+        name:"Amayra's Tracker",
+        description:"A habit tracker plateform build in ReactJS. With pomodoro, archive  and many tracker functionalities.",
+        tags:["react","api","mockbee"],
+        
+        demo:"https://amayrastracker.netlify.app/",
+        github:""
+    },
+    {
+        id:4,
+        name:"Crome Extension",
+        description:"A cool looking extension to increase your productivity.",
+        tags:["react","crome"],
+        
+        demo:"https://chrome.google.com/webstore/detail/amayras-tab/pngdjolcchamoihdmcboabhpkmmgkcdi",
+        github:""
+       
+    },{
+        id:5,
+        name:"Wish UI",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["html & css","CL"],
+        
+        demo:"https://wish-ui.netlify.app/",
+        github:""
+        
+    },
+
+] 
